28a694d8cd689d6ea5647e07a2114ab381a5857f
Melotwo <turoka15@gmail.com>
2025-11-15 08:30:10 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/caterpro-ai/',
+  base: '/CaterPro-AI/',
   plugins: [react()],
 })
