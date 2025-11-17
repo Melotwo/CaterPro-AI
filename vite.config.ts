8dea5e31d0bfa12e40600fe2ce145687c1fa33bb
Melotwo <turoka15@gmail.com>
2025-11-17 08:14:16 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CaterPro-AI/',
   plugins: [react()],
+  define: {
+    // Expose environment variables to the client
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  }
 })
