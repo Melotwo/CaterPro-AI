7ec2290a986c2218b62a3dda21ee502c0912d2ee
Melotwo <turoka15@gmail.com>
2026-03-03 11:25:11 +0200

    Add service worker for CaterPro AI

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+// Service worker for CaterPro AI
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Default fetch handler
+});
