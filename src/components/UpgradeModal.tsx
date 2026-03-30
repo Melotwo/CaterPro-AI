a9d01393de07fd5f407093a68c9015a650fb7059
Melotwo <turoka15@gmail.com>
2026-03-30 14:32:23 +0200

    Update UpgradeModal.tsx

--- src/components/UpgradeModal.tsx
+++ src/components/UpgradeModal.tsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useRef, useState } from 'react';
-import { SubscriptionPlan } from './AppSubscription';
+import { SubscriptionPlan } from './App';
 
 interface UpgradeModalProps {
   isOpen: boolean;
