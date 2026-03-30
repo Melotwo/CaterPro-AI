478646967aa9c5651b8b8156143b962862e46972
Melotwo <turoka15@gmail.com>
2026-03-30 11:03:24 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 
 // --- INFRASTRUCTURE IMPORTS ---
-import { useAuth } from './useAuth.tsx';
-import { useAppSubscription } from './useAppSubscription.ts';
+import { useAuth } from './AuthContext';
+import { useAppSubscription } from './AppSubscription';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import AuthModal from './AuthModal';
