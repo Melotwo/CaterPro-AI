44b0034354e0a6aaeda927c8dd82755927f2225b
Melotwo <turoka15@gmail.com>
2026-03-31 10:24:35 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Index from "./pages/Index"; 
+// Pointing to the new filename to break the cache
+import Home from "./pages/Home"; 
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -9,7 +10,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Index />} />
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
